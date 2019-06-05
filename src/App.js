258305b42264aca2639afe47224532b4d16b4852
pclevy <pclevybr@gmail.com>
2019-06-04 21:56:46 -0300

    Botões 'Sacola ....' adiconados

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+      <div id="Group" className="group">
+        <div id="Group2" className='group2'>
+          <div id="Menu" className='menu'>
+          <button id="Sacola" className='menu-item1'>
+              SACOLA
+            </button>        
+            <button id="Pagamento" className='menu-item2'>
+              PAGAMENTO
+            </button>        
+            <button id="Confirmacao" className='menu-item3'>
+              CONFIRMAÇÂO
+            </button>        
+            {/*}
+            <div id="Group3" className='menu-item1'>
+              SACOLA
+            </div>        
+            <div id="Group3" className='menu-item2'>
+              PAGAMENTO
+            </div>        
+            <div id="Group3" className='menu-item3'>
+              CONFIRMAÇÂO
+            </div>        
+            */}
+          </div>
+        </div>
       </div>
     );
   }
